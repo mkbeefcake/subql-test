09147f9f59a6bedf0b4fcea714da886b8ece92f0
mkbeefcake <mkblockchaindev@outlook.com>
2023-08-18 18:07:27 +0800

    20230818: updated the rpc to joystream

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,16 +3,27 @@ import {
   SubstrateEvent,
   SubstrateBlock,
 } from "@subql/types";
-import { Account, Transfer } from "../types";
+import { Account, Transfer, BlockEntity, ExtrinsicEntity } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 import { decodeAddress } from "@polkadot/util-crypto";
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
   // Do something with each block handler here
+  const blockEntity: BlockEntity = BlockEntity.create({
+    id: block.block.header.hash.toString(),
+    blockNumber: block.block.header.number.toNumber(),
+    timestamp: block.timestamp.toString()
+  });
+  await blockEntity.save();
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  // Do something with a call handler here
+  const extrinsicEntity = ExtrinsicEntity.create({
+    id: extrinsic.block.block.header.hash.toString(),
+    blockNumber: extrinsic.block.block.header.number.toNumber(),
+    timestamp: extrinsic.block.timestamp.toString()
+  })
+  await extrinsicEntity.save();
 }
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
